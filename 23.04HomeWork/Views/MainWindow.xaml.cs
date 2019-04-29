df2e619a38e3bcd0c60cc34878693359826dd7d8
Anar Axmed <anar.axmed5514@gmail.com>
2019-04-29 21:55:56 +0400

    last

---
+++ 23.04HomeWork/Views/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using _23._04HomeWork.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace _23._04HomeWork.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            GroupViewModel groupViewModel = new GroupViewModel();
+
+            Groups groups = new Groups(groupViewModel);
+
+            groups.ShowDialog();
+
+        }
+    }
+}
